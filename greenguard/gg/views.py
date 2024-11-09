c7bad5609a0f1c0a58ae55030042f0a7eb082b38
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-09 16:24:19 +0000

    Nov 9, 2024, 8:24 PM

---
+++ greenguard/gg/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
