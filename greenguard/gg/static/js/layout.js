8d3776d783590d6ca3c14b4784fe97a5561d2cb8
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-10 10:19:26 +0000

    Nov 10, 2024, 2:19 PM

--- greenguard/gg/static/js/layout.js
+++ greenguard/gg/static/js/layout.js
@@ -4,10 +4,27 @@ const span1 = document.getElementById("span1");
 const span2 = document.getElementById("span2");
 const span3 = document.getElementById("span3");
 
-let countdown = 0;
+let is_ready = true;
 
-let open = False;
+let open = false;
 
 burgermenu.addEventListener("click", function() {
-    
+    if (is_ready == true) {
+        if (open == false) {
+            span1.style.animation = "span1_open ease 1s";
+            span2.style.animation = "span2_open ease 1s";
+            span3.style.animation = "span3_open ease 1s";
+
+            span1.addEventListener("animationend", function() {
+                span1.style.animation = '';
+                span2.style.animation = '';
+                span3.style.animation = '';
+                span1.style.top = "20px";
+                span2.style.opacity = "0";
+                span3.style.top = '-20px';
+            })
+        } else {
+
+        }
+    }
 })
