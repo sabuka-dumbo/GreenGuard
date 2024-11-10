7ac39acfda557125450aef35d9aa72ac2397dd1a
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-10 08:14:56 +0000

    Nov 10, 2024, 12:14 PM

--- greenguard/greenguard/settings.py
+++ greenguard/greenguard/settings.py
@@ -116,11 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "gg.User"
+
+
+STATIC_URL = '/static/'
