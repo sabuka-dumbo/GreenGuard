4f83334d9a2ea83486712a66d52b6940dcf336a1
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-10 10:14:10 +0000

    Nov 10, 2024, 2:14 PM

---
+++ greenguard/gg/static/js/layout.js
@@ -0,0 +1,13 @@
+const navbar = document.getElementById("navbar");
+const burgermenu = document.getElementById("burgermenu");
+const span1 = document.getElementById("span1");
+const span2 = document.getElementById("span2");
+const span3 = document.getElementById("span3");
+
+let countdown = 0;
+
+let open = False;
+
+burgermenu.addEventListener("click", function() {
+    
+})
