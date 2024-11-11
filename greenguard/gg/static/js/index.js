0da44ebc04a254a956b7824748c61186e929200b
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-11 12:15:01 +0000

    Nov 11, 2024, 4:15 PM

--- greenguard/gg/static/js/index.js
+++ greenguard/gg/static/js/index.js
@@ -1,5 +1,8 @@
 const line = document.getElementById("main_page_line");
 const text = document.getElementById("main_page_text");
+const icon = document.getElementById("main_page_icon");
 
 text.style.color = "#DA8359";
 line.style.color = "#DA8359";
+icon.classList = "bi bi-house-door navbar-icon";
+icon.innerHTML = `<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>`
