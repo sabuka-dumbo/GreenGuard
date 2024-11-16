fdf5fccf2bc24e47c837ac75833a3f216f8e2479
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-16 15:59:25 +0000

    Nov 16, 2024, 7:59 PM

--- greenguard/gg/static/js/index.js
+++ greenguard/gg/static/js/index.js
@@ -61,3 +61,28 @@ usage_value.addEventListener("focusout", function() {
         })
     }
 })
+
+
+
+
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
