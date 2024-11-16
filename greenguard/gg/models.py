f112c22d5614cce02f6667afe8c74fe2852524e4
Sabuka-Dumbo <anonym.sas@proton.me>
2024-11-16 07:31:08 +0000

    Nov 16, 2024, 11:31 AM

--- greenguard/gg/models.py
+++ greenguard/gg/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Usage(models.Model):
+    usage_type = models.CharField(max_length=150)
+    
